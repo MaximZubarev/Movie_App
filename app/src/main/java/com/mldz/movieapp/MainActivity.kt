4bc518a4a2770541f28c9a0cc8e6db9c8af8e016
Maxim Zubarev <maxim@numbuster.com>
2021-10-19 17:15:43 +0300

    start project and create movie details page

---
+++ app/src/main/java/com/mldz/movieapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.mldz.movieapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener{
+            run {
+                startActivity(Intent(this, MovieDetailsActivity::class.java))
+            }
+        }
+    }
+}
