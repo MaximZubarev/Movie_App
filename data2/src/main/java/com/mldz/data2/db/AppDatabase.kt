85d6a5cc464c795019b9559c4f4b11301958473a
Maxim <maxim@numbuster.com>
2023-03-07 18:01:11 +0300

    increase versions

--- data2/src/main/java/com/mldz/data2/db/AppDatabase.kt
+++ data2/src/main/java/com/mldz/data2/db/AppDatabase.kt
@@ -7,7 +7,8 @@ import com.mldz.data2.entities.MovieEntity
 
 @Database(
     entities = [MovieEntity::class],
-    version = 1
+    version = 1,
+    exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun movieDao(): MovieDao
